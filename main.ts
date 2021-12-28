a4a7ceb0bedc1825964a75368880f4a85f18adb0
Oliver Bullock <kinetic-coder@users.noreply.github.com>
2021-12-28 18:25:52 +0000

    manage lights

--- main.ts
+++ main.ts
@@ -1,14 +1,17 @@
 function processRequest (requestString: string) {
     if (requestString == "G") {
+        switchOffLights()
         moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(0)
+        moveMotorZIP.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.White))
         moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(1)
-        moveMotorZIP.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red))
+        moveMotorZIP.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.White))
         moveMotorZIP.show()
         Kitronik_Move_Motor.beepHorn()
         Kitronik_Move_Motor.beepHorn()
         Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 80)
     }
     if (requestString == "S") {
+        switchOffLights()
         Kitronik_Move_Motor.beepHorn()
         Kitronik_Move_Motor.stop()
     }
@@ -23,10 +26,20 @@ function processRequest (requestString: string) {
         Kitronik_Move_Motor.stop()
     }
 }
+function switchOffLights () {
+    moveMotorZIP2 = Kitronik_Move_Motor.createMoveMotorZIPLED(0)
+    moveMotorZIP.clear()
+    moveMotorZIP2 = Kitronik_Move_Motor.createMoveMotorZIPLED(1)
+    moveMotorZIP.clear()
+    moveMotorZIP2 = Kitronik_Move_Motor.createMoveMotorZIPLED(2)
+    moveMotorZIP.clear()
+    moveMotorZIP2 = Kitronik_Move_Motor.createMoveMotorZIPLED(3)
+}
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
     processRequest(receivedString)
 })
+let moveMotorZIP2: Kitronik_Move_Motor.MoveMotorZIP = null
 let moveMotorZIP: Kitronik_Move_Motor.MoveMotorZIP = null
 radio.setGroup(50)
 basic.forever(function () {
